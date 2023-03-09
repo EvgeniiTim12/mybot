4ddfe5662b870acb64427f9315148ac56a3d4744
Пользователь <gegitym@gmail.com>
2023-03-09 21:58:45 +0200

    fixim

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token="5930715308:AAFL2QdL-G-p1mF61uUr8sKRK-nRWfpgnFg"
-botName="Payment_Adolf_bot"
+token="6028043689:AAE4qdFf_v7WKsPaz6wCk46rL_Fwpm5w688"
+botName="payment_test_casino_bot"
 SPREADSHEET_ID='1wpawHPkTmqhccKS-440bS_DZHcjHX0ZIo0zfMWMI4Yk'
 comman=['/cum','/cancel','/start','/reload','/add_admin','/remove_admin']
